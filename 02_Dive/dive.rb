d3225c83d26e6b931f6e65fee35208894a42eec3
Joe Thor <joethor1989@gmail.com>
2021-12-02 09:35:47 -0600

    Update dive.rb

--- 02_Dive/dive.rb
+++ 02_Dive/dive.rb
@@ -22,10 +22,44 @@ def part1
     end
     puts "depth"
     puts depth
-    puts 'horiztonal'
+    puts 'horizontal'
     puts horizontal
     puts 'product'
     puts horizontal * depth
 end
 
-part1()
+def part2
+
+    lines = File.readlines("data.txt") 
+
+    depth = 0
+    horizontal = 0
+    aim = 0
+    lines.each do |line|
+        direction, value = line.split
+
+
+        if direction == 'forward'
+            horizontal += value.to_i
+            depth_calculation = value.to_i * aim
+            depth += depth_calculation
+        elsif direction == 'down'
+            aim += value.to_i
+
+        elsif direction == 'up'
+            aim -= value.to_i
+
+        else
+            puts 'OOPS'
+        end
+    end
+
+    
+    puts "depth"
+    puts depth
+    puts 'horizontal'
+    puts horizontal
+    puts 'product'
+    puts horizontal * depth
+end
+part2()
