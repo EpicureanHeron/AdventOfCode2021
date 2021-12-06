e26f38f76195cd828bd31af7bf7d14564a03e1d3
Joe Thor <joethor1989@gmail.com>
2021-12-06 15:59:48 -0600

    Add files via upload

---
+++ 06/6.rb
@@ -0,0 +1,36 @@
+class FishSchool
+    attr_reader :day
+    def initialize_state(day)
+    @day = 0
+
+    end
+
+    def increment_day
+
+    end
+
+    def update_state_of_school
+
+    end
+
+    def generate_new_fish
+
+    end
+
+end
+
+class Fish
+
+    def life_counter_value
+
+    end
+
+    def regenerate?
+
+    end
+
+    def increment_counter
+
+    end
+
+end
